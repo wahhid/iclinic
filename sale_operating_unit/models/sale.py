76b967a8baf86308d7e58a59304fce8b0027c46e
weha <wahhid@weha-id.com>
2022-01-12 23:04:21 +0700

    update

--- sale_operating_unit/models/sale.py
+++ sale_operating_unit/models/sale.py
@@ -41,19 +41,19 @@ class SaleOrder(models.Model):
                 self.operating_unit_id:
             self.team_id = False
 
-    @api.multi
-    @api.constrains('team_id', 'operating_unit_id')
-    def _check_team_operating_unit(self):
-        for rec in self:
-            _logger.info(rec.team_id)
-            _logger.info(rec.team_id.operating_unit_id)
-            _logger.info(rec.operating_unit_id)
-            if (rec.team_id and
-                    rec.team_id.operating_unit_id != rec.operating_unit_id):
-                raise ValidationError(_('Configuration error\n'
-                                        'The Operating Unit of the sales team '
-                                        'must match with that of the '
-                                        'quote/sales order'))
+    # @api.multi
+    # @api.constrains('team_id', 'operating_unit_id')
+    # def _check_team_operating_unit(self):
+    #     for rec in self:
+    #         _logger.info(rec.team_id)
+    #         _logger.info(rec.team_id.operating_unit_id)
+    #         _logger.info(rec.operating_unit_id)
+    #         if (rec.team_id and
+    #                 rec.team_id.operating_unit_id != rec.operating_unit_id):
+    #             raise ValidationError(_('Configuration error\n'
+    #                                     'The Operating Unit of the sales team '
+    #                                     'must match with that of the '
+    #                                     'quote/sales order'))
 
     @api.multi
     @api.constrains('operating_unit_id', 'company_id')
